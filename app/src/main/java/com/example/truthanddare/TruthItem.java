3b0b799379c0adcc85de192504be11fb8a6a183a
sarojluffy <Sarojreus10@gmail.com>
2023-03-02 23:48:49 +0545

    final

---
+++ app/src/main/java/com/example/truthanddare/TruthItem.java
@@ -0,0 +1,14 @@
+package com.example.truthanddare;
+public class TruthItem{
+
+    private String mText;
+
+    public TruthItem(String text) {
+        mText = text;
+    }
+
+    public String getmText() {
+        return mText;
+    }
+
+}
---
+++ app/src/main/java/com/example/truthanddare/Values.java
@@ -0,0 +1,28 @@
+package com.example.truthanddare;
+
+public class Values {
+
+    String[] truths = new String[]{
+            "tapaiko type ko kta ya kti kasto hola?",
+            "k tapai internet bina basna saknu huncha?",
+            "k tapai relation ma hunuhuncha?",
+            "tapai ko ex ko nam k ho ?",
+            "tapai hapta ma kati patak nuhaunu huncha?",
+            "maya ra paisa ma k thulo?",
+            "k tapaile kasailai cheat garnu vayeko cha??",
+            "tapaiko bank ma kati paisa cha?"
+    };
+
+    String[] dares = new String[]{
+            "tapailai man parne manche lai call garnu hos",
+            "tapaile le sabai ko khaja ko paisa tirnu hos",
+            "tapaia voli kta/kti ko outfit lagayera aunu hos",
+            "tapaile  1 ghanta ko lagi phone prayog nagarnu hos",
+            "aja mero assignment gardinu hos",
+            "tapaiko girlfriend snga ko chat dekhaunu hos",
+            "tapai arko class ma gayera  period basnu hos",
+            "tell me something you havent told your parents",
+            "do 20 pushups"
+    };
+}
+
